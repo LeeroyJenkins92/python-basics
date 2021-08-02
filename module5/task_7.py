165de858229593445ce76c124e2f157b37784d4d
Mad Max <admin@admin.local>
2021-08-02 22:49:39 +0300

    module 5 fixed + module 6 finished

--- module5/task_7.py
+++ module5/task_7.py
@@ -7,7 +7,7 @@ num3 = int(input('Введите 3e число: '))
 
 if (num1 == num2) and (num1 == num3) :
     print(3)
-elif (num2 == num3) or (num1 == num3) :
+elif (num1 == num2) or (num1 == num3) or (num2 == num3) :
     print(2)
 else:
     print(0)
