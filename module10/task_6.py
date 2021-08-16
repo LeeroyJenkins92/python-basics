76e4c01dd92b54f80881ddab4bf9525e68256dbe
Mad Max <admin@admin.local>
2021-08-17 00:22:30 +0300

    10/6_fix

--- module10/task_6.py
+++ module10/task_6.py
@@ -7,12 +7,14 @@ print()
 n = int(input('Введите число N: '))
 f = 1
 summ = 0
-count = 0
+#count = 0
 
 for fac in range(1, n + 1):
-        for num in range(1, fac + 1):
-                while num > count:
-                        f *= num
-                        count += 1
-                        summ += f
+    f *= fac
+    summ += f
+        # for num in range(1, fac + 1):
+        #         while num > count:
+        #                f *= num
+        #                count += 1
+        #                summ += f
 print("Сумма факториалов данного числа: ",summ)
